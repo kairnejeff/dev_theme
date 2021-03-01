160cc1dca2a0836bf31a88b8c60d958191004e94
jing <jing@karinejeff.fr>
2021-03-01 09:17:22 +0100

    first message

---
+++ assets/js/custom.js
@@ -0,0 +1,47 @@
+/*
+ * Custom code goes here.
+ * A template should always ship with an empty custom.js
+ */
+let tabs = $('section.checkout-step')
+let steps = $('.checkout-bar .step')
+
+function displayCheckOutTab() {
+    $('section.checkout-step.-unreachable').css('display', 'none')
+    $('section.checkout-step.-reachable:not(.-current)').css('display', 'none')
+}
+
+function removeCurrentClass() {
+    let i = 0
+    let remove = false
+    while (!remove && i < tabs.length) {
+        if ($(tabs[i]).hasClass('-current')) {
+            $(tabs[i]).removeClass('-current')
+            remove = true
+        }
+        i++;
+
+    }
+}
+
+function addCompleteCssProgressBar() {
+    tabs.each(function(index) {
+        if ($(tabs[index]).hasClass('-complete')) {
+            $(steps[index]).addClass('complete')
+        }
+    })
+}
+
+(function() {
+    addCompleteCssProgressBar()
+    displayCheckOutTab()
+    steps.each(function(index) {
+        $(this).click(function() {
+            if ($(tabs[index]).hasClass('-complete')) {
+                removeCurrentClass()
+                $(tabs[index]).css('display', 'block')
+                $(tabs[index]).addClass("-current")
+                displayCheckOutTab()
+            }
+        })
+    })
+})();
