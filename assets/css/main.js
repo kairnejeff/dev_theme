89d5986b1a0d28206a5eb681013a79c8855b06ae
jing <jing@karinejeff.fr>
2021-03-04 12:10:00 +0100

    change

--- assets/css/main.js
+++ assets/css/main.js
@@ -16,29 +16,17 @@
   \******************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-<<<<<<< HEAD
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../../../../../scss/app.scss */ \"./scss/app.scss\");\n/* harmony import */ var _scss_header_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../../../../scss/header.scss */ \"./scss/header.scss\");\n/* harmony import */ var _scss_home_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../../../../scss/home.scss */ \"./scss/home.scss\");\n\n\n\n\n//# sourceURL=webpack://assets/./index.js?");
-=======
-<<<<<<< HEAD
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../scss/app.scss */ \"./scss/app.scss\");\n/* harmony import */ var _scss_header_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../scss/header.scss */ \"./scss/header.scss\");\n/* harmony import */ var _scss_home_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../scss/home.scss */ \"./scss/home.scss\");\n/* harmony import */ var _scss_footer_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../scss/footer.scss */ \"./scss/footer.scss\");\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://assets/./index.js?");
-=======
-<<<<<<< HEAD
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../../../../../scss/app.scss */ \"./scss/app.scss\");\n/* harmony import */ var _scss_header_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../../../../scss/header.scss */ \"./scss/header.scss\");\n/* harmony import */ var _scss_home_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../../../../scss/home.scss */ \"./scss/home.scss\");\n\n\n\n\n//# sourceURL=webpack://assets/./index.js?");
-=======
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../scss/app.scss */ \"./scss/app.scss\");\n/* harmony import */ var _scss_header_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../scss/header.scss */ \"./scss/header.scss\");\n/* harmony import */ var _scss_home_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../scss/home.scss */ \"./scss/home.scss\");\n\r\n\r\n\n\n//# sourceURL=webpack://assets/./index.js?");
->>>>>>> 949dfdeac20e01abad0c587fbcf8912449ca4c37
->>>>>>> refs/remotes/origin/master
->>>>>>> f43b37f1b80dfa84b53899a353edad63e6728cfa
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_header_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../scss/header.scss */ \"./scss/header.scss\");\n/* harmony import */ var _scss_home_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../scss/home.scss */ \"./scss/home.scss\");\n/* harmony import */ var _scss_footer_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../scss/footer.scss */ \"./scss/footer.scss\");\n/* harmony import */ var _scss_categorie_grid_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../scss/categorie-grid.scss */ \"./scss/categorie-grid.scss\");\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://assets/./index.js?");
 
 /***/ }),
 
-/***/ "./scss/app.scss":
-/*!***********************!*\
-  !*** ./scss/app.scss ***!
-  \***********************/
+/***/ "./scss/categorie-grid.scss":
+/*!**********************************!*\
+  !*** ./scss/categorie-grid.scss ***!
+  \**********************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://assets/./scss/app.scss?");
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://assets/./scss/categorie-grid.scss?");
 
 /***/ }),
 
