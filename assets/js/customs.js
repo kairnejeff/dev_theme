4070e54c29d64b243a681eaea1af32373d47bb57
jing <jing@karinejeff.fr>
2021-07-28 15:06:43 +0200

    "home product carousel icone"

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -340,7 +340,7 @@ $(document).ready(function(e) {
         rewind: false,
         items: 1,
         smartSpeed: 70,
-        navText: ['<svg viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>', '<svg viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg>']
+        navText: ['<svg viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" fill="white"></path></svg>', '<svg viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" fill="white"></path></svg>']
     });
 })
 
