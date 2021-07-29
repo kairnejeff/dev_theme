5502bb193228a47e53d9eabad42dc6aa3a82728e
jing <jing@karinejeff.fr>
2021-07-29 11:50:42 +0200

    "truspilote nav pev next"

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -350,7 +350,7 @@ $(document).ready(function(e) {
         rewind: false,
         items: 1,
         smartSpeed: 70,
-        navText: ['<svg viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" fill="white"></path></svg>', '<svg viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" fill="white"></path></svg>']
+        navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"]
     });
 })
 
