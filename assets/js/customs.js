3f2dc2af12ccd2fc01a5760c381edb8f19c0ef81
Lucie <lucie.lalo@icloud.com>
2021-09-15 17:51:32 +0200

    retours en vrac

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -450,5 +450,101 @@ $(document).ready(function(e) {
 
 $('.add-card-button').on('click', 'button:not([type="submit"])', function(e){
     e.preventDefault();
-  })
+})
 
+// $(document).ready(function(e){
+//     // duration of scroll animation
+//     var scrollDuration = 300;
+//     // paddles
+//     var leftPaddle = document.getElementsByClassName('left-paddle');
+//     var rightPaddle = document.getElementsByClassName('right-paddle');
+//     // get items dimensions
+//     var itemsLength = $('.menu-tab').length;
+//     var itemSize = $('.menu-tab').outerWidth(true);
+//     // get some relevant size for the paddle triggering point
+//     var paddleMargin = 20;
+
+//     // get wrapper width
+//     var getMenuWrapperSize = function() {
+//         return $('#main-menu').outerWidth();
+//     }
+//     var menuWrapperSize = getMenuWrapperSize();
+//     // the wrapper is responsive
+//     $(window).on('resize', function() {
+//         menuWrapperSize = getMenuWrapperSize();
+//     });
+//     // size of the visible part of the menu is equal as the wrapper size 
+//     var menuVisibleSize = menuWrapperSize;
+
+//     // get total width of all menu items
+//     var getMenuSize = function() {
+//         return itemsLength * itemSize;
+//     };
+//     var menuSize = getMenuSize();
+//     // get how much of menu is invisible
+//     var menuInvisibleSize = menuSize - menuWrapperSize;
+
+//     // get how much have we scrolled to the left
+//     var getMenuPosition = function() {
+//         return $('.info-detailed #main-menu ul').scrollLeft();
+//     };
+
+//     // finally, what happens when we are actually scrolling the menu
+//     $('.info-detailed #main-menu ul').on('scroll', function() {
+
+//         // get how much of menu is invisible
+//         menuInvisibleSize = menuSize - menuWrapperSize;
+//         // get how much have we scrolled so far
+//         var menuPosition = getMenuPosition();
+
+//         var menuEndOffset = menuInvisibleSize - paddleMargin;
+
+//         // show & hide the paddles 
+//         // depending on scroll position
+//         if (menuPosition <= paddleMargin) {
+//             $(leftPaddle).addClass('hidden');
+//             $(rightPaddle).removeClass('hidden');
+//         } else if (menuPosition < menuEndOffset) {
+//             // show both paddles in the middle
+//             $(leftPaddle).removeClass('hidden');
+//             $(rightPaddle).removeClass('hidden');
+//         } else if (menuPosition >= menuEndOffset) {
+//             $(leftPaddle).removeClass('hidden');
+//             $(rightPaddle).addClass('hidden');
+//     }
+
+
+//     });
+
+//     // scroll to left
+//     $(rightPaddle).on('click', function() {
+//         $('.info-detailed #main-menu ul').animate( { scrollLeft: menuInvisibleSize}, scrollDuration);
+//     });
+
+//     // scroll to right
+//     $(leftPaddle).on('click', function() {
+//         $('.info-detailed #main-menu ul').animate( { scrollLeft: '0' }, scrollDuration);
+//     });
+// });
+
+
+function ScrollMobileCarousel() {
+    
+    $('.info-detailed #main-menu ul').addClass("owl-carousel")
+    $('.info-detailed #main-menu ul').owlCarousel({
+        items: 1,
+        loop: false,
+        nav: true,
+        margin: 10,
+        dots: false,
+        autoplay: false,
+        autoplayTimeout: 10000,
+        autoplayHoverPause: true,
+        navText: ['<svg viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>', '<svg viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg>']
+    })
+}
+$(document).ready(function(e) {
+    ScrollMobileCarousel()
+    $(window).resize(ScrollMobileCarousel)
+
+})
