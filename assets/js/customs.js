b8b6f77e3912387096ae795204400ae24c65c565
Lucie <lucie.lalo@icloud.com>
2021-09-20 14:01:16 +0200

    Page produit : onglet avis à  afficher

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -545,6 +545,12 @@ function scrollProductCarousel() {
         loop: false,
         nav: true,
         rewind: false,
+        responsive: {
+            0: { items: 4 },
+            480: { items: 4 },
+            600: { items: 4 },
+            1000: { items: 4 }
+        },
         smartSpeed: 70,
         slideBy: 'page',
         navText: ['<svg viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>', '<svg viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg>']
